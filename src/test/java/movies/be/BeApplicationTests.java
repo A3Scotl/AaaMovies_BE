05782c4ab4e2d1a5c3c851f166803efe771151ed
Nguyen Truong An <nguyentruongan0610@gmail.com>
2025-05-04 17:39:26 +0700

    Init project

---
+++ src/test/java/movies/be/BeApplicationTests.java
@@ -0,0 +1,13 @@
+package movies.be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
